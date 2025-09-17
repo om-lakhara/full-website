ee0aef326a8cb4003210b9ee1a1eea1873838044
om-lakhara <omlakhara153@gmail.com>
2025-09-17 11:58:10 +0530

    client id replaced

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_3lWi20EHN', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '43k4g7oc3mj3geaufebku7iir7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: 'q1d3l6l6nk83a56m98jjvhc5p', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
